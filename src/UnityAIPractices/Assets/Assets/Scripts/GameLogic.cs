d963a47e98b2facbc020656a83cf53a590436ade
Fernando J Rojas <korkstr@gmail.com>
2017-12-18 15:02:19 -0500

    Updated project to unity 2017.2.1f1 and added new class to separate game logic from board model too use multiple game types to test AI against

---
+++ src/UnityAIPractices/Assets/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Enums;
+using GameConsts;
+
+
+namespace GameLogic
+{
+
+    public class BaseLogic
+    {
+        //memebers
+        protected PlayerIndex m_currentPlayer;
+
+        //methods
+        public int CheckVictory()
+        {
+            if (isGameOver() != GameOver.TIE)
+            {
+                if (m_currentPlayer == PlayerIndex.PLAYER1) return GameOver.PLAYER1;
+                if (m_currentPlayer == PlayerIndex.PLAYER2) return GameOver.PLAYER2;
+            }
+            else
+            {
+                return GameOver.TIE;
+            }
+
+            return GameOver.NO_VAL;
+        }
+
+        public void NextTurn()
+        {
+            m_currentPlayer = (m_currentPlayer == PlayerIndex.PLAYER1) ? PlayerIndex.PLAYER2 : PlayerIndex.PLAYER1;
+        }
+
+        protected int isGameOver()
+        {
+            //used for override
+            return GameOver.TIE;
+        }
+        
+
+
+    }
+
+}
